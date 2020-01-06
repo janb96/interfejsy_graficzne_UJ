8de076a5efb0670f9bf1a3e711f9ed910ba27369
Rafal Lebioda <lebioda.rafal@gmail.com>
2020-01-06 16:32:52 +0100

    [NF] New view - language

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './components/home/Home';
 import V2 from './components/v2/V2';
+import Language from './components/Language/Language';
 
 import './App.css';
 class App extends Component {
@@ -13,6 +14,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/' component={Home}/>
             <Route exact path='/v2' component={V2}/>
+            <Route exact path='/language' component={Language}/>
           </Switch>
         </BrowserRouter>
     );
