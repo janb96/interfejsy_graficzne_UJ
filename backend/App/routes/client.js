376ae771c31924760521a30114ec60303c32cf3d
ROG\piotr <piotr.dunaj95@gmail.com>
2020-01-16 16:28:30 +0100

    [NF] add card activation

--- backend/App/routes/client.js
+++ backend/App/routes/client.js
@@ -5,6 +5,7 @@ let ClientModel = require("../model/Client");
 let ApiUtils = require('../utils/ApiUtils');
 let TokenValidator = require('../utils/TokenValidator');
 
+//ONLY FOR TESTS
 router.get('/', TokenValidator, function (req, res, next) {
     let cardId = req.cardId;
 
@@ -44,7 +45,7 @@ router.get('/balance', TokenValidator, function (req, res, next) {
 });
 
 //TODO test when database will be up
-router.patch('/pin', TokenValidator, function (req, res, next) {
+router.patch('/card/pin', TokenValidator, function (req, res, next) {
     let cardId = req.cardId;
 
     let oldPinCode = req.body.oldPinCode;
@@ -102,6 +103,11 @@ router.patch('/pin', TokenValidator, function (req, res, next) {
                 return;
             }
 
+            if (!client.isActive) {
+                ApiUtils.sendApiError(res, 500, "Card is not active.");
+                return;
+            }
+
             if (client.pinCode !== oldPinCode) {
                 ApiUtils.sendApiError(res, 500, "PIN code is invalid.");
                 return;
@@ -119,6 +125,98 @@ router.patch('/pin', TokenValidator, function (req, res, next) {
 
                         if (data.nModified !== 1) {
                             ApiUtils.sendApiError(res, 500, "PIN code was not changed.");
+                            return;
+                        }
+
+                        ApiUtils.sendApiResponse(res, 200, true)
+                    });
+        });
+});
+
+//TODO test when database will be up
+router.post('/card/activate', TokenValidator, function (req, res, next) {
+    let cardId = req.cardId;
+
+    let oldPinCode = req.body.oldPinCode;
+    let newPinCode = req.body.newPinCode;
+
+    if (oldPinCode === undefined) {
+        ApiUtils.sendApiError(res, 500, "Field 'oldPinCode' could not be empty!");
+        return;
+    }
+
+    if (!/^\d+$/.test(oldPinCode)) {
+        ApiUtils.sendApiError(res, 500, "Field 'oldPinCode' has to have only digits!");
+        return;
+    }
+
+    if (oldPinCode.length < config.minPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'oldPinCode' has to have minimum " + config.minPinDigits + " digits!");
+        return;
+    }
+
+    if (oldPinCode.length > config.maxPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'oldPinCode' has to have maximum " + config.maxPinDigits + " digits!");
+        return;
+    }
+
+    if (newPinCode === undefined) {
+        ApiUtils.sendApiError(res, 500, "Field 'newPinCode' could not be empty!");
+        return;
+    }
+
+    if (!/^\d+$/.test(newPinCode)) {
+        ApiUtils.sendApiError(res, 500, "Field 'newPinCode' has to have only digits!");
+        return;
+    }
+
+    if (newPinCode.length < config.minPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'newPinCode' has to have minimum " + config.minPinDigits + " digits!");
+        return;
+    }
+
+    if (newPinCode.length > config.maxPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'newPinCode' has to have maximum " + config.maxPinDigits + " digits!");
+        return;
+    }
+
+    ClientModel
+        .findOne({cardId: cardId}, function (error, client) {
+            if (error) {
+                ApiUtils.sendApiError(res, 500, error.message);
+                return;
+            }
+
+            if (!client) {
+                ApiUtils.sendApiError(res, 500, "Could not get client with card ID = " + cardId + " from credit cards providers databases");
+                return;
+            }
+
+            if (client.isActive) {
+                ApiUtils.sendApiError(res, 500, "Card was activated already.");
+                return;
+            }
+
+            if (client.pinCode !== oldPinCode) {
+                ApiUtils.sendApiError(res, 500, "PIN code is invalid.");
+                return;
+            }
+
+            ClientModel
+                .update(
+                    {cardId: cardId},
+                    {
+                        pinCode: newPinCode,
+                        isActive: true
+                    },
+                    function (error, data) {
+                        if (error) {
+                            ApiUtils.sendApiError(res, 500, error.message);
+                            return;
+                        }
+
+                        if (data.nModified !== 1) {
+                            ApiUtils.sendApiError(res, 500, "Card is active now.");
                             return;
                         }
 
