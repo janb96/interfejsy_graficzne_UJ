4a83ebc56d82ff451ee983271c3ed63fe53134b2
ROG\piotr <piotr.dunaj95@gmail.com>
2020-01-16 16:14:02 +0100

    [NF] refactor auth, add pin change

--- backend/App/routes/auth.js
+++ backend/App/routes/auth.js
@@ -4,10 +4,12 @@ let jwt = require('jsonwebtoken');
 
 let config = require('../config');
 
+let ClientModel = require("../model/Client");
 let ApiUtils = require('../utils/ApiUtils');
 
 router.post('/authenticate', function (req, res, next) {
     let cardId = req.body.cardId;
+    let pinCode = req.body.pinCode;
 
     if (cardId === undefined) {
         ApiUtils.sendApiError(res, 500, "Field 'cardId' could not be empty!");
@@ -24,9 +26,54 @@ router.post('/authenticate', function (req, res, next) {
         return;
     }
 
-    let token = jwt.sign({cardId: cardId}, config.jwtSecret, {expiresIn: config.jwtTime});
+    if (pinCode === undefined) {
+        ApiUtils.sendApiError(res, 500, "Field 'pinCode' could not be empty!");
+        return;
+    }
 
-    ApiUtils.sendApiToken(res, 200, token);
+    if (!/^\d+$/.test(pinCode)) {
+        ApiUtils.sendApiError(res, 500, "Field 'pinCode' has to have only digits!");
+        return;
+    }
+
+    if (pinCode.length < config.minPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'pinCode' has to have minimum " + config.minPinDigits + " digits!");
+        return;
+    }
+    
+    if (pinCode.length > config.maxPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'pinCode' has to have maximum " + config.maxPinDigits + " digits!");
+        return;
+    }
+
+    ClientModel
+        .findOne({cardId: cardId}, function (error, client) {
+            if (error) {
+                ApiUtils.sendApiError(res, 500, error.message);
+                return;
+            }
+
+            if (!client) {
+                ApiUtils.sendApiError(res, 500, "Could not get client with card ID = " + cardId + " from credit cards providers databases");
+                return;
+            }
+
+            if (client.isBlocked) {
+                ApiUtils.sendApiError(res, 500, "Card " + cardId + " is blocked. Card will be lock in ATM.");
+                return;
+            }
+
+            if (client.pinCode !== pinCode) {
+                ApiUtils.sendApiError(res, 500, "PIN code is invalid.");
+                return;
+            }
+
+            //TODO check expiration date
+
+            let token = jwt.sign({cardId: cardId}, config.jwtSecret, {expiresIn: config.jwtTime});
+
+            ApiUtils.sendApiToken(res, 200, token);
+        });
 });
 
 module.exports = router;
--- backend/App/routes/client.js
+++ backend/App/routes/client.js
@@ -1,8 +1,6 @@
 let express = require('express');
 let router = express.Router();
 
-let config = require('../config');
-
 let ClientModel = require("../model/Client");
 let ApiUtils = require('../utils/ApiUtils');
 let TokenValidator = require('../utils/TokenValidator');
@@ -26,50 +24,6 @@ router.get('/', TokenValidator, function (req, res, next) {
         });
 });
 
-router.get('/auth', TokenValidator, function (req, res, next) {
-    let cardId = req.cardId;
-
-    let pinCode = req.body.pinCode;
-
-    if (!pinCode) {
-        ApiUtils.sendApiError(res, 500, "Field 'pinCode' could not be empty!");
-        return;
-    }
-
-    if (!/^\d+$/.test(pinCode)) {
-        ApiUtils.sendApiError(res, 500, "Field 'pinCode' has to have only digits!");
-        return;
-    }
-
-    if (pinCode.length < config.minPinDigits) {
-        ApiUtils.sendApiError(res, 500, "Field 'pinCode' has to have minimum " + config.minPinDigits + " digits!");
-        return;
-    }
-    if (pinCode.length > config.maxPinDigits) {
-        ApiUtils.sendApiError(res, 500, "Field 'pinCode' has to have maximum " + config.maxPinDigits + " digits!");
-        return;
-    }
-
-    ClientModel
-        .findOne({cardId: cardId}, function (error, client) {
-            if (error) {
-                ApiUtils.sendApiError(res, 500, error.message);
-                return;
-            }
-
-            if (!client) {
-                ApiUtils.sendApiError(res, 500, "Could not get client with card ID = " + cardId + " from credit cards providers databases");
-                return;
-            }
-
-            if (client.pinCode === pinCode) {
-                ApiUtils.sendApiResponse(res, 200, true)
-            } else {
-                ApiUtils.sendApiResponse(res, 200, false)
-            }
-        });
-});
-
 router.get('/balance', TokenValidator, function (req, res, next) {
     let cardId = req.cardId;
 
@@ -89,28 +43,89 @@ router.get('/balance', TokenValidator, function (req, res, next) {
         });
 });
 
-// router.patch('/pin', TokenValidator, function (req, res, next) {
-//     let cardId = req.cardId;
-//
-//     let oldPinCode = req.body.oldPinCode;
-//     let newPinCode = req.body.newPinCode;
-//
-//     ClientModel
-//         .findOne({cardId: cardId}, function (error, client) {
-//             if (error) {
-//                 ApiUtils.sendApiError(res, 500, error.message);
-//                 return;
-//             }
-//
-//             if (!client) {
-//                 ApiUtils.sendApiError(res, 500, "Could not get client with card ID = " + cardId + " from credit cards providers databases");
-//                 return;
-//             }
-//
-//
-//             ApiUtils.sendApiResponse(res, 200, client.balance)
-//         });
-// });
+//TODO test when database will be up
+router.patch('/pin', TokenValidator, function (req, res, next) {
+    let cardId = req.cardId;
+
+    let oldPinCode = req.body.oldPinCode;
+    let newPinCode = req.body.newPinCode;
+
+    if (oldPinCode === undefined) {
+        ApiUtils.sendApiError(res, 500, "Field 'oldPinCode' could not be empty!");
+        return;
+    }
+
+    if (!/^\d+$/.test(oldPinCode)) {
+        ApiUtils.sendApiError(res, 500, "Field 'oldPinCode' has to have only digits!");
+        return;
+    }
+
+    if (oldPinCode.length < config.minPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'oldPinCode' has to have minimum " + config.minPinDigits + " digits!");
+        return;
+    }
+
+    if (oldPinCode.length > config.maxPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'oldPinCode' has to have maximum " + config.maxPinDigits + " digits!");
+        return;
+    }
+
+    if (newPinCode === undefined) {
+        ApiUtils.sendApiError(res, 500, "Field 'newPinCode' could not be empty!");
+        return;
+    }
+
+    if (!/^\d+$/.test(newPinCode)) {
+        ApiUtils.sendApiError(res, 500, "Field 'newPinCode' has to have only digits!");
+        return;
+    }
+
+    if (newPinCode.length < config.minPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'newPinCode' has to have minimum " + config.minPinDigits + " digits!");
+        return;
+    }
+
+    if (newPinCode.length > config.maxPinDigits) {
+        ApiUtils.sendApiError(res, 500, "Field 'newPinCode' has to have maximum " + config.maxPinDigits + " digits!");
+        return;
+    }
+
+    ClientModel
+        .findOne({cardId: cardId}, function (error, client) {
+            if (error) {
+                ApiUtils.sendApiError(res, 500, error.message);
+                return;
+            }
+
+            if (!client) {
+                ApiUtils.sendApiError(res, 500, "Could not get client with card ID = " + cardId + " from credit cards providers databases");
+                return;
+            }
+
+            if (client.pinCode !== oldPinCode) {
+                ApiUtils.sendApiError(res, 500, "PIN code is invalid.");
+                return;
+            }
+
+            ClientModel
+                .update(
+                    {cardId: cardId},
+                    {pinCode: newPinCode},
+                    function (error, data) {
+                        if (error) {
+                            ApiUtils.sendApiError(res, 500, error.message);
+                            return;
+                        }
+
+                        if (data.nModified !== 1) {
+                            ApiUtils.sendApiError(res, 500, "PIN code was not changed.");
+                            return;
+                        }
+
+                        ApiUtils.sendApiResponse(res, 200, true)
+                    });
+        });
+});
 
 //ONLY FOR TESTS - DO NOT USE
 router.post('/', function (req, res, next) {
